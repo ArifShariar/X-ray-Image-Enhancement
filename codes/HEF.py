c7418f6ddeb82701dcfdf7f60f49132abf4e81eb
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-11-20 22:36:56 +0600

    updated codes in HEF.py . Also added main function and moved the code under a function.

--- codes/HEF.py
+++ codes/HEF.py
@@ -23,7 +23,6 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import fft2, ifft2, fftshift
 from collections import OrderedDict
 
-
 def convert_to_grayscale(image_input):
     """
     Convert the image to grayscale
@@ -69,52 +68,57 @@ def histogram(data):
     return np.array(list(_hist.values())), np.array(list(_hist.keys()))
 
 
-image = imageio.imread('/home/arif/PycharmProjects/X-ray-Image-Enhancement/data/010.jpg')
+def hef(image_in: np.ndarray) -> np.ndarray:
+    # image = imageio.imread('D:\\Pycharm\\X-ray-Image-Enhancement\\data\\010.jpg')
 
-# convert the image to grayscale
-image_greyscale = convert_to_grayscale(image)
+    # convert the image to grayscale
+    image_greyscale = convert_to_grayscale(image_in)
 
-image = normalize_image(np.min(image_greyscale), np.max(image), 0, 255, image_greyscale)
+    image_in = normalize_image(np.min(image_greyscale), np.max(image_in), 0, 255, image_greyscale)
 
-# compute the FFT of the image
-image_fft = fft2(image)
-image_sfft = fftshift(image_fft)
+    # compute the FFT of the image
+    image_fft = fft2(image_in)
+    image_sfft = fftshift(image_fft)
 
-m, n = image_sfft.shape
-filter_array = np.zeros((m, n))
+    m, n = image_sfft.shape
+    filter_array = np.zeros((m, n))
 
-dov = 30
-# compute the HFE filter
-for i in range(m):
-    for j in range(n):
-        filter_array[i, j] = 1.0 - np.exp(-((i - m / 2.0) ** 2 + (j - n / 2.0) ** 2) / (2 * (dov ** 2)))
+    dov = 30
+    # compute the HFE filter
+    for i in range(m):
+        for j in range(n):
+            filter_array[i, j] = 1.0 - np.exp(-((i - m / 2.0) ** 2 + (j - n / 2.0) ** 2) / (2 * (dov ** 2)))
 
-k1 = 0.5
-k2 = 0.75
+    k1 = 0.5
+    k2 = 0.75
 
-high_filter = k1 + k2 * filter_array
+    high_filter = k1 + k2 * filter_array
 
-image_filtered = image_sfft * high_filter
+    image_filtered = image_sfft * high_filter
 
-image_hef = np.real(ifft2(fftshift(image_filtered)))
+    image_hef = np.real(ifft2(fftshift(image_filtered)))
 
-# histogram equalization
-hist, bins = histogram(image_hef)
+    # histogram equalization
+    hist, bins = histogram(image_hef)
 
-# calculate the probability density function
-pixel_probability = hist / hist.sum()
+    # calculate the probability density function
+    pixel_probability = hist / hist.sum()
 
-# calculate the cumulative distribution function
-cdf = np.cumsum(pixel_probability)
-cdf_normalized = cdf * 255
-hist_eq = {}
-for i in range(len(cdf)):
-    hist_eq[bins[i]] = int(cdf_normalized[i])
+    # calculate the cumulative distribution function
+    cdf = np.cumsum(pixel_probability)
+    cdf_normalized = cdf * 255
+    hist_eq = {}
+    for i in range(len(cdf)):
+        hist_eq[bins[i]] = int(cdf_normalized[i])
 
-for i in range(m):
-    for j in range(n):
-        image[i, j] = hist_eq[image_hef[i, j]]
+    for i in range(m):
+        for j in range(n):
+            image_in[i, j] = hist_eq[image_hef[i, j]]
 
-imageio.imwrite('/home/arif/PycharmProjects/X-ray-Image-Enhancement/data/010_hef.jpg', image)
-plt.imshow(image.astype(np.uint8), cmap='gray')
-plt.show()
+    return image_in.astype(np.uint8)
+
+
+if __name__ == '__main__':
+    image = imageio.imread('D:\\Pycharm\\X-ray-Image-Enhancement\\data\\010.jpg')
+    image = hef(image)
+    imageio.imwrite('D:\\Pycharm\\X-ray-Image-Enhancement\\data\\010_hef_new.jpg', image)
