0be94872e4e2dbccc490687c2c6fd337b420a22d
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-11-22 03:17:58 +0600

    Significantly changed Benchmark.py, removed path from the main file and added them in directory.py. Also added code for plotting graph from csv file ( ComparisonPlot.py). Anyway, in Benchmark.py, HEF benchmarking is not working properly

---
+++ benchmarks/ComparisonPlot.py
@@ -0,0 +1,37 @@
+import csv
+import matplotlib.pyplot as plt
+import codes.directory
+
+
+def plot_graphs():
+    # open csv file
+    with open(codes.directory.parent_dir + codes.directory.csv_dir + 'covid_time_count_UM.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        um_covid = {}
+
+        for row in reader:
+            if row:
+                um_covid[int(row[0])] = float(row[1])
+
+    with open(codes.directory.parent_dir + codes.directory.csv_dir + 'covid_time_count_CLAHE.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        clahe_covid = {}
+
+        for row in reader:
+            if row:
+                clahe_covid[int(row[0])] = float(row[1])
+
+    # plot graphs
+    plt.plot(list(um_covid.keys()), list(um_covid.values()), label="UM")
+    plt.plot(list(clahe_covid.keys()), list(clahe_covid.values()), label="CLAHE")
+    plt.xlabel('Number of images')
+    plt.ylabel('Time taken')
+    plt.title('Time taken for Covid files')
+    plt.legend()
+    plt.grid()
+    plt.savefig(codes.directory.parent_dir + codes.directory.csv_dir + 'comparison.png')
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_graphs()
---
+++ codes/directory.py
@@ -0,0 +1,18 @@
+############################
+#
+# This file is for managing the directories
+# I was facing problem with the relative path, so I created this python file
+# to have all directories in one place
+# if someone wants to change the directory, they can do it here
+#
+############################
+
+import os
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\\"
+covid_dir = "data\\COVID-19\\"
+non_covid_dir = "data\\Non-COVID-19\\"
+
+csv_dir = "benchmarks\\"
+
+
